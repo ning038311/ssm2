75fc6cd121ad5324313ddbb54f4f24345f5a18f0
Ning <Ning@qq.com>
2019-11-27 20:09:36 +0800

    修改java

--- git/src/com/ning/git/FirstClass.java
+++ git/src/com/ning/git/FirstClass.java
@@ -17,6 +17,7 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         //hello test
+        //
     }
 
     public void test(){
